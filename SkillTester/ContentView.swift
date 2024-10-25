16f7948b703bf3396a7e64f15b5a15846d962064
IanTranPersonal <vinhxtran1@gmail.com>
2024-10-25 18:55:19 +1100

    Initial Commit

---
+++ SkillTester/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SkillTester
+//
+//  Created by Vinh Tran on 25/10/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SkillTester/SkillTesterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkillTesterApp.swift
+//  SkillTester
+//
+//  Created by Vinh Tran on 25/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct SkillTesterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
